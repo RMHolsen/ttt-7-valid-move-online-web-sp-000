8d0220e084b0232621af6349927c62fab0c645b5
Rosario Holsen <rholsen@gmail.com>
2020-08-07 16:54:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,6 @@ def valid_move?
   input = gets.strip.to_i 
   if input > 8 || input < 0 
     return false 
-  position_taken?
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
